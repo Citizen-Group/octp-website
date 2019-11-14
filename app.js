0af7dad78d22741c8152ff29f7e2d8be6f75f439
shimavitz <evillepecon@gmail.com>
2019-11-14 00:28:08 -0500

    Favicon v1

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var index = require('./routes/index');
 var app = express();
 
 // Favicon
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 // view engine setup    
 app.set('views', path.join(__dirname, 'views'));
